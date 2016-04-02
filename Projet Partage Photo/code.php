9dfb63da71b9cf014b7d560b2590ba763c51e102
Achille Duchemin <achille.duchemin@gmail.com>
2016-04-02 13:42:24 +0200

    Code 02/04/2016

---
+++ Projet Partage Photo/code.php
@@ -0,0 +1,15 @@
+
+<<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8"/>
+  <title> Formulaire php</title>
+</head>
+<body>
+<form action="cible.php" method="POST">
+  <p> <label> Prénom : <input type="text" name "prenom" /></label></p>
+  <p> <input type="submit"/></p>
+
+</body>
+</html>
